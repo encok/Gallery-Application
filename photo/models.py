1e556596c5443b9b94463d30304a8ec72d536043
enock <enckkipronoh@gmail.com>
2021-10-08 21:18:45 +0300

    creating category model

--- photo/models.py
+++ photo/models.py
@@ -1,6 +1,16 @@
 from django.db import models
 
+#location model
 class Location(models.Model):
+    name = models.CharField(max_length =50)
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['name']
+
+#category model
+class Category(models.Model):
     name = models.CharField(max_length =50)
     def __str__(self):
         return self.name
