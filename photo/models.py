dba7689e61170e8f613c2a1a430e81a943e38ed2
enock <enckkipronoh@gmail.com>
2021-10-08 21:17:02 +0300

    creating class meta

--- photo/models.py
+++ photo/models.py
@@ -2,4 +2,8 @@ from django.db import models
 
 class Location(models.Model):
     name = models.CharField(max_length =50)
-    
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['name']
