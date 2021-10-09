d00384f28b8e9b7dd180389fe358e5b37ca775c1
enock <enckkipronoh@gmail.com>
2021-10-09 15:32:01 +0300

    making path for images in the url

--- Gallery/settings.py
+++ Gallery/settings.py
@@ -126,8 +126,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/images/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
+    BASE_DIR , "static"
 ]
+MEDIA_ROOT = BASE_DIR = 'static/images'
+STATIC_ROOT = BASE_DIR ='staticfiles'
+
 DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
 
