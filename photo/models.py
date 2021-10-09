7bce7d49016d69dd7f73f18ec6b17408cbefa568
enock <enckkipronoh@gmail.com>
2021-10-09 08:42:28 +0300

    adding the admin page

--- photo/models.py
+++ photo/models.py
@@ -6,22 +6,51 @@ class Location(models.Model):
     def __str__(self):
         return self.name
 
-    class Meta:
-        ordering = ['name']
+    def save_location(self):
+        self.save()
 
+    def delete_location(self):
+        self.delete()
+
+  
 #category model
 class Category(models.Model):
     name = models.CharField(max_length =50)
+
     def __str__(self):
         return self.name
 
-    class Meta:
-        ordering = ['name']
+    def save_category(self):
+        self.save()
+
+    def delete_category(self):
+        self.delete()
+
 
 
 #image model
 class Image(models.Model):
     name = models.CharField(max_length = 60)
     description = models.TextField()
-    image_location = models.ForeignKey('Location')
-    image_category = models.ForeignKey('Category')
+    image_location = models.ForeignKey('Location' ,on_delete=models.CASCADE, null=True)
+    image_category = models.ForeignKey('Category' ,on_delete=models.CASCADE, null=True)
+    pub_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+    
+    def save_image(self):
+        self.save()
+
+    def delete_image(self):
+        self.delete()
+
+    class Meta:
+        ordering = ['name']
+
+    @classmethod
+    def update_image(cls, id ,name, description , image_location, image_category):
+        update = cls.objects.filter(id = id).update(name = name, description = description ,image_location = image_location,image_category = image_category)
+        return update
+
+    
--- photo/tests.py
+++ photo/tests.py
@@ -1,3 +1,40 @@
+
 from django.test import TestCase
+from .models import Image,Location,Category
 
 # Create your tests here.
+class ImageTestClass(TestCase):
+
+    # Set up method
+    def setUp(self):
+        self.cat = Category(name="fashion")
+        self.cat.save_category()
+
+        self.loc = Location(name="Africa")
+        self.loc.save_location()
+        # self.alabama= Image(name = 'Tour', description ='American Tour')
+        self.alabama = Image(name='image test', description='my test',image_location=self.loc, image_category=self.cat)
+        self.alabama.save_image()
+    # Testing  instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.alabama,Image))
+
+    # Testing Save Method
+    def test_save_method(self):
+        self.alabama.save_image()
+        images = Image.objects.all()
+        self.assertTrue(len(images) > 0)
+    
+    
+    def tearDown(self):
+        self.alabama.delete_image()
+        self.cat.delete_category()
+        self.loc.delete_location()
+
+    def test_update_image(self):
+        self.alabama.save_image()
+        alabama = Image.update_image( self.alabama.id, 'test update', 'my test',self.loc, self.cat)
+        upimage = Image.objects.filter(id = self.alabama.id)
+        print(upimage)
+        print(alabama)
+        self.assertTrue(Image.name == 'test update')
