dbab2599524592e09a5e947509f392219476fb2b
enock <enckkipronoh@gmail.com>
2021-10-09 12:28:19 +0300

    adding urls app

--- Gallery/urls.py
+++ Gallery/urls.py
@@ -17,8 +17,10 @@ from django.conf.urls import url,include
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
+from django.urls import path,include
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    path('admin/', admin.site.urls),
+    path('', include('photos.urls')),
    
 ]
