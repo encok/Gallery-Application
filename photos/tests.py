abb8d5e87101f070256bd3f1e12b54d2d5128cb4
enock <enckkipronoh@gmail.com>
2021-10-10 14:37:54 +0300

    writing tests for the second model

--- photos/tests.py
+++ photos/tests.py
@@ -4,6 +4,41 @@ from .models import Category,Photo,Location
 
 
 # Create your tests here.
+
+class LocationTestCLass(TestCase):
+    #Set up Method
+    def setUp(self):
+        self.loc = Location(name="Africa")
+        self.loc.save_location()
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.loc,Location))
+
+    def test_save_method(self):
+        self.loc.save_location()
+        locations = Location.objects.all()
+        self.assertTrue(len(locations) > 0)
+
+    def test_delete_method(self):
+        self.loc.save_location()
+        self.loc.delete_location()
+        location = Location.objects.all()
+        self.assertTrue(len(location) == 0)
+
+class CategoryTestClass(TestCase):
+    # Set up Method
+    def setUp(self):
+        self.cat = Category(name="fashion")
+        self.cat.save_category()
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.cat, Category))
+
+    def test_save_method(self):
+        self.cat.save_category()
+        category = Category.objects.all()
+        self.assertTrue(len(category) > 0)
+
 class PhotoTestClass(TestCase):
 
     # Set up method
