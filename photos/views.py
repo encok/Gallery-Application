dbead5c376198c0cef138baa4a126f0d1c2be143
enock <enckkipronoh@gmail.com>
2021-10-10 09:11:47 +0300

    creating links to filter images depending on category

--- photos/views.py
+++ photos/views.py
@@ -5,11 +5,15 @@ from .models import Category, Photo
 # Create your views here.
 def gallery(request):
     category = request.GET.get('category')
-    print('category:', category)
+    # print('category:', category)
+    if category == None:
+        photos = Photo.objects.all()
+        
+    else:
+        photos = Photo.objects.filter(category__name=category)
 
-
+    
     categories = Category.objects.all()
-    photos = Photo.objects.all()
     context = {'categories': categories, 'photos': photos}
     return render(request,'photos/gallery.html', context)
 
