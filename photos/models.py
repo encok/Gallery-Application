73a07579c4ed03c2145bba46f72f4e6f36e28601
enock <enckkipronoh@gmail.com>
2021-10-10 12:51:00 +0300

    writing first test

--- photos/models.py
+++ photos/models.py
@@ -9,6 +9,12 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
+class Location(models.Model):
+    name = models.CharField(max_length=100, null=False, blank=False)
+
+    def __str__(self):
+        return self.name
+
 class Photo(models.Model):
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True)
     image = models.ImageField(null=False, blank=False)
--- photos/tests.py
+++ photos/tests.py
@@ -1,3 +1,21 @@
+
 from django.test import TestCase
+from .models import Category,Photo,Location
+
 
 # Create your tests here.
+class PhotoTestClass(TestCase):
+
+    # Set up method
+    def setUp(self):
+        self.photo= Photo(description ='Photo of my wife and i  in mount Albert in Colorado')
+
+        # Testing  instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.photo,Photo))
+
+        # Testing Save Method
+    def test_save_method(self):
+        self.photo.save_photo()
+        images = Photo.objects.all()
+        self.assertTrue(len(images) > 0)
